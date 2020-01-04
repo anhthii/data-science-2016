c03024e6f6c9e7705895f4fe1b582d543add358a
HoangSinh <hoangsinh95nd@gmail.com>
2020-01-04 20:24:09 +0700

    Add files via upload
    refactor crawl.py

--- crawl.py
+++ crawl.py
@@ -1,42 +1,62 @@
 from requests_html import HTMLSession
-import threading
-import time
-import random
 import csv
 import numpy as np
-
+import pandas as pd
 
 session = HTMLSession()
 r = session.get('https://tiki.vn/')
+r.encoding = 'utf-8'
 
 data = []
+link_category_dict = dict()
 
 def parse_link(href, currentPage = 1):
+    page = href+"&page="+str(currentPage)
     _session = HTMLSession()
-    _r = _session.get(href)
-    category_tag = _r.html.find(".breadcrumb span", first=True)
+    _r = _session.get(page)
     items = _r.html.find('.content .title')
-    print("page = ", currentPage)
 
     if len(items) == 0:
         return
     for item in items:
-        data.append([item.text.rstrip("."), category_tag.text])
+        category = link_category_dict[href]
+        data.append([item.text.strip().rstrip("."), category])
     nextPage = currentPage+1
-    time.sleep(1.0)
-    parse_link(href+"&page="+str(nextPage), nextPage)
-    
+    try:
+        print("page ", page)
+        parse_link(href, nextPage)
+    except Exception as e:
+        print("some errors occured", str(e))
 
-items = r.html.find('li.MenuItem-tii3xq-0 a')
-for item in items:
-    href = item.attrs["href"]
-    time.sleep(0.2)
-    parse_link(href)
+def get_categories():
+    categories = []
+    item_list = r.html.find('li.MenuItem-tii3xq-0 ')
+    for item in item_list:
+        href = item.find('a', first=True).attrs["href"]
+        category = item.text
+        categories.append((href, category))
+        link_category_dict[href] = category
+    return categories
 
-np.random.shuffle(data)
+def crawl_data():
+    category_tuples = get_categories()
+    for category_tuple in category_tuples:
+        (href, category_name) = category_tuple
+        parse_link(href)
 
-with open('product.csv', 'w', newline='', encoding='utf-8') as file:
-    writer = csv.writer(file)
-    writer.writerow(["product_title", "category"])
-    for item in data:
-        writer.writerow(item)
+def save_data():
+    np.random.shuffle(data)
+    if len(data) != 0:
+        with open('product.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerow(["product_title", "category"])
+            for item in data:
+                writer.writerow(item)
+
+category_tuples = get_categories()
+def run():
+    print("start crawling")
+    crawl_data()
+    save_data()
+
+run()
